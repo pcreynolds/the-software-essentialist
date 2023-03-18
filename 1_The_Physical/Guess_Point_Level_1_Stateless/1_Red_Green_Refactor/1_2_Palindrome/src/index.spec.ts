6d9afb8a41cb8e05a50f91ed8adc91a519869f07
pcreynolds <preynolds04@yahoo.com>
2023-03-18 13:10:42 -0400

    Add palindrome method and check return value is boolean

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,9 @@
+import {palindrome} from "./index";
 
 describe('palindrome checker', () => {
+
+    it("should return a boolean",()=>{
+        expect(typeof palindrome()).toBe("boolean");
+    })
 
 })
