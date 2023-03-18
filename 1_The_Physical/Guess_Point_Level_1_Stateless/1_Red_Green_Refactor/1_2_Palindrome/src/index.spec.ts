bfa11cc71c0562de3c1ad70db31772e80da4f381
pcreynolds <preynolds04@yahoo.com>
2023-03-18 13:29:16 -0400

    Update check to allow for dynamic input values

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -6,6 +6,10 @@ describe('palindrome checker', () => {
         expect(typeof palindrome()).toBe("boolean");
     })
 
+    it("returns true fro \"a\"",()=>{
+        expect(palindrome("a")).toBe(true);
+    })
+
     it("returns true for mom",()=>{
         expect(palindrome("mom")).toBe(true);
     })
@@ -13,5 +17,13 @@ describe('palindrome checker', () => {
     it("returns false when passed bill",()=>{
         expect(palindrome("bill")).toBe(false);
     })
+
+    it("returns true for \"Was It A Rat I Saw\" ",()=>{
+        expect(palindrome("Was It A Rat I Saw" )).toBe(true);
+    })
+
+    it("returns true for \"Never Odd or Even\"",()=>{
+        expect(palindrome("Never Odd or Even")).toBe(true);
+    });
 
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,7 +1,18 @@
-export function palindrome(value?:string):boolean{
+export function palindrome(value:string = ""):boolean{
 
-    if(value === "bill"){
-        return false;
+    if(value.length < 1) return false;
+    value = value.toLowerCase().replace(/\s/g,"");
+
+    let headIndex = 0;
+    let tailIndex = value.length-1;
+
+    while(headIndex < tailIndex){
+        const head = value[headIndex++];
+        const tail = value[tailIndex--];
+        console.log(value,head,tail);
+        if(head !== tail){
+            return false;
+        }
     }
 
     return true;
